5cbde0632b93f88f596e0745425ea597cfb4231f
lucy-kevin <kayikayikevin7@gmail.com>
2024-06-28 13:21:39 +0300

    enums

--- classes/emumerated types/enum.dart
+++ classes/emumerated types/enum.dart
@@ -1,3 +1,23 @@
 void main(List<String> args) {
-  
+ //helps you create similar objects in one structure
+  final woof =  Animal(name: "Woof", type: AnimalType.dog);
+
+  if (woof.type == AnimalType.dog){
+    print("Woof is a dog");
+
+  }else{
+    print("Woof is not a dog");
+  }
+}
+enum AnimalType{ rabbit, dog, cat,}
+
+class Animal{
+
+  final String name;
+  final AnimalType type;
+
+  const Animal({
+    required this.name,
+    required this.type,
+  });
 }
